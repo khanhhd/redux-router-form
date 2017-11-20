03bbc7c790fb2a01190171b9162f8dab8952988a
khanhhd <hoang.dang.khanh@framgia.com>
2017-11-20 18:01:57 +0700

    stop at 135

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
 
 
 class PostNew extends Component {
+  renderTitleField(field) {
+    return(
+      <div className="form-group">
+        <lable>{field.lable}</lable>
+        <input className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div>
-        <div className="text-xs-right">
-          <Link className="btn btn-primary" to="/posts">
-            Post list
-          </Link>
-        </div>
-        Post new!
-      </div>
+      <form>
+        <Field
+          lable="Title"
+          name="title"
+          component={this.renderTitleField}
+        />
+        <Field
+          lable="Tag"
+          name="Tag"
+          component={this.renderTitleField}
+        />
+        <Field
+          lable="Content"
+          name="content"
+          component={this.renderTitleField}
+        />
+      </form>
     )
   }
 }
 
-export default PostNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostNew);
